15d9dfbcc73468976c3341df027624d6fa865026
Lucas Henrique <lucasmonte2011@gmail.com>
2021-03-17 09:15:31 -0300

    Update files

--- Atividade02/ex01.hs
+++ Atividade02/ex01.hs
@@ -1,5 +1,23 @@
 --Lista de exercícios sobre funções recursivas
-fatorial :: Int -> Int
-fatorial 0 = 1
-fatorial n  
-    | n > 0 = n * fatorial (n - 1)
+--resolução mais detalhada
+data Valor = Indeterminado | Val Integer deriving Show
+instance Num Valor where
+    Val x + Val y = Val (x+y)
+    Val x * Val y = Val (x*y)
+    abs (Val n) = Val (abs n)
+    signum (Val n) = Val (signum n)
+    fromInteger n = Val n
+    negate (Val n) = Val (negate n)
+
+fatorial :: Valor -> Valor
+fatorial (Val n)  
+    | n < 0 = Indeterminado
+    | n == 0 || n == 1 = Val 1
+    | n > 0 = Val n * fatorial (Val (n - 1))
+
+--resolução simples
+fatorial' :: Int -> Int 
+fatorial' n
+    |n < 0 = 0
+    | n == 0 || n == 1 = 1
+    | otherwise  = n * fatorial' (n-1)
--- Atividade05/ex01.hs
+++ Atividade05/ex01.hs
@@ -5,6 +5,6 @@ putStr' (x:xs) = do putChar x
                     putStr' xs
 
 --aplicação com compreensão de lista e sequence
-putStr :: String -> IO ()
-putStr [] = return ()
-putStr (x:xs) = []
+putStr_ :: String -> IO ()
+putStr_ [] = return ()
+putStr_ (x:xs) = sequence_ []
